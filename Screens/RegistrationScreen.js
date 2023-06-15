9202bee61bd0c9a838e00d2dc55e5102aae26d0c
NadiaZaichenko <nadyazaichenko884@gmail.com>
2023-06-15 21:15:10 +0300

    first add

---
+++ Screens/RegistrationScreen.js
@@ -0,0 +1,23 @@
+// // import BGImage from './Image/Photo BG.png';
+// import { View,ImageBackground, StyleSheet} from 'react-native';
+
+// const RegistrationScreen = () => {
+//     return(
+//         <View style={styles.container}>
+//             {/* <ImageBackground source={BGImage} style={styles.image}/> */}
+
+//         </View>
+//     )
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//     },
+//     image: {
+//       flex: 1,
+//       justifyContent: 'center',
+//     },
+// })
+
+// export default RegistrationScreen;
