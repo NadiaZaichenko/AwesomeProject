7a1dcdf6583998527c1b1467f54f78e7b09b0bb1
NadiaZaichenko <nadyazaichenko884@gmail.com>
2023-07-05 12:31:32 +0300

    added hw

--- App.js
+++ App.js
@@ -1,25 +1,8 @@
-// import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,  View, ImageBackground} from 'react-native';
 import RegistrationScreen from './Screens/RegistrationScreen';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ImageBackground style={styles.image} source={require('./Screens/Image/PhotoBG.jpg')}></ImageBackground>
       <RegistrationScreen />
-      {/* <StatusBar style="auto" /> */}
-    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // fontFamily: "Roboto",
-  },
-  image: {
-    flex: 1,
-    justifyContent: "flex-end", 
-    resizeMode: "cover",
-  },
-});
--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -1,74 +1,136 @@
-import React from 'react';
-import { View, Image, TouchableOpacity, StyleSheet, Text, TextInput, } from 'react-native';
+import React, {useState} from 'react';
+import { View, Image, TouchableOpacity, StyleSheet, Text, TextInput, ImageBackground, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard} from 'react-native';
+import { AntDesign } from '@expo/vector-icons'; 
+import { useFonts } from 'expo-font';
+import * as Font from 'expo-font';
 
-const RegistrationScreen = () => (
+const RegistrationScreen = () => {
+  const initialState = {
+    name: '',
+    email: '',
+    password: '',
+  }
+
+  const [showBoard, setShowBoard] = useState(false);
+  const [state, setState] = useState(initialState);
+  const [isPasswordSecure, setIsPasswordSecure] = useState(true);
+
+  function submitForm (){
+    setShowBoard(false);
+    Keyboard.dismiss();
+    console.log(state);
+    setState(initialState);
+  }
+
+  const [fontsLoaded] = useFonts({
+    'Roboto-Bold': require('../assets/fonts/RobotoCondensed-Bold.ttf'),
+    'Roboto-Regular': require('../assets/fonts/RobotoCondensed-Regular.ttf')
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  };
+
+     return (
+      <TouchableWithoutFeedback
+       onPress={() => Keyboard.dismiss()}>
+
         <View style={styles.container}>
+
+            <ImageBackground style={styles.image} source={require('../assets/images/PhotoBG.jpg')}>
+
+            <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            keyboardVerticalOffset={Platform.OS === 'ios' ? -97 : -97}
+          >
+            <View style={styles.form}>
 
             <View style={styles.photoContainer}>
                 <Image />
-            </View>
                 <TouchableOpacity style={styles.singBtn}>
-                   <Text style={styles.buttonSing} >
-                    &#10011;
-                   </Text>
+                <AntDesign name="pluscircleo" size={24} color="#FF6C00" />
                 </TouchableOpacity>
-    
-            <View style={styles.form}>
+            </View>
+              
+              <Text style={styles.textTitle}>Реєстрація</Text>
+          
+              <View style={{paddingTop: 16}}>
+                <TextInput 
+                style={styles.input} 
+                placeholder='Логін' 
+                value={state.name}
+                onFocus={() => setShowBoard(true)}
+                onChangeText={(value) => setState((prevState) => ({...prevState, name: value}))}/>
+              
+                <TextInput 
+                style={styles.input} 
+                placeholder='Адреса електронної пошти'
+                value={state.email}
+                onFocus={() => setShowBoard(true)}
+                onChangeText={(value) => setState((prevState) => ({...prevState, email: value}))}/>
 
-              <View style={{ marginTop: 92 }}>
-                <Text style={styles.textTitle}>Реєстрація</Text>
+                <TextInput 
+                style={styles.input} 
+                secureTextEntry={isPasswordSecure} placeholder='Пароль' 
+                value={state.password}
+                onFocus={() => setShowBoard(true)}
+                onChangeText={(value) => setState((prevState) => ({...prevState, password: value}))}/>
+                <TouchableOpacity activeOpacity={0.6} onPress ={() => setIsPasswordSecure(!isPasswordSecure)}>
+                  <Text style={styles.inputPassword}>{isPasswordSecure ? "Показати" : "Сховати"}</Text>
+                </TouchableOpacity>
               </View>
-
-              <View style={{marginTop: 33}}>
-                <TextInput style={styles.input} placeholder='Логін'/>
-              </View>
-
-              <View style={{marginTop: 16}}>
-                <TextInput style={styles.input} placeholder='Адреса електронної пошти'/>
-              </View>
-
-              <View style={{marginTop: 16}}>
-                <TextInput style={styles.input} secureTextEntry={true} placeholder='Пароль' />
-              </View>
-              <TouchableOpacity style={styles.registBtn}>
-                   <Text style={styles.buttonRegist}>Зареєструватися</Text>
+              <TouchableOpacity activeOpacity={0.6} style={styles.registBtn} onPress={submitForm}>
+                   <Text style={styles.buttonText}>Зареєструватися</Text>
                 </TouchableOpacity>
 
-                <View style={{marginTop: 16, marginBottom: 66}}>
+                <View style={{marginTop: 16,}}>
                     <Text style={{textAlign: 'center', color: '#1B4371'}}>Вже є акаунт? Увійти</Text>
                 </View>
              </View>
-            
-
+            </KeyboardAvoidingView>
+          </ImageBackground>
         </View>
-        
-    );
+      </TouchableWithoutFeedback>  
+     )  
+};
 
 const styles = StyleSheet.create({
     container: {
-      position: "relative",
+      flex: 1, 
+      // justifyContent: "flex-end",
       backgroundColor: "#fff",
-      borderTopRightRadius: 25,
-      borderTopLeftRadius: 25,
+    },
+    image: {
+      flex: 1, 
+      resizeMode: "cover",
+      justifyContent: "flex-end",
     },
     photoContainer: {
-        position: "absolute",
-        width: 120,
-        height: 120,
-        left: 128,
-        top: -60,
-        backgroundColor: '#F6F6F6',
-        borderRadius: 16,
+      position: "relative",
+      width: 120,
+      height: 120,
+      marginTop: -60,
+      marginLeft: "auto",
+      marginRight: "auto",
+      backgroundColor: "#F6F6F6",
+      borderRadius: 16,
     },
-      form: {
-        marginHorizontal: 16,
-      },
+    form: {
+      overflow: "visible",
+      backgroundColor: "#fff",
+      borderTopLeftRadius: 25,
+      borderTopRightRadius: 25,
+      paddingLeft: 16,
+      paddingRight: 16,
+      paddingBottom: 45,
+    },
       textTitle: {
         textAlign: 'center',
         fontWeight: 500,
         fontSize: 30,
         color: '#212121',
         lineHeight: 35,
+        marginTop: 33,
     },
       input: {
         borderWidth: 1,
@@ -78,26 +140,16 @@ const styles = StyleSheet.create({
         height: 50,
         padding: 16,
         color: '#212121',
-      },
-      buttonSing: {
-        color: '#FF6C00',
+        marginTop: 16,
       },
       singBtn:{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        zIndex: 1,
-        position: 'absolute',
-        top: 21,
-        left: 235,
-        width: 25,
-        height: 25,
-        borderWidth: 1,
-        borderColor: "#FF6C00",
-        borderRadius: 25,
+        position: "absolute",
+        bottom: 16,
+        right: -12,
         backgroundColor: "#FFFFFF",
+        borderRadius: 50,
       },
-      buttonRegist: {
+      buttonText: {
         alignItems: 'center',
         paddingTop: 16,
         paddingBottom: 16,
@@ -111,7 +163,17 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         backgroundColor: '#FF6C00',
         borderRadius: 100,
-        marginTop: 43,
+        marginTop: 27,
+      },
+      inputPassword: {
+        position: 'absolute',
+        zIndex: 99,
+        top: -35,
+        right: 15,
+        color: "#1B4371",
+        fontSize: 14,
+        lineHeight: 18.75
+
       }
     
 })
