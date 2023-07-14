8ab1c9ea1543cc78bb69752b14843cf8440124f3
NadiaZaichenko <nadyazaichenko884@gmail.com>
2023-07-14 14:17:24 +0300

    some added

--- Screens/LoginScreen/LoginScreen.js
+++ Screens/LoginScreen/LoginScreen.js
@@ -1,14 +1,36 @@
 import React, { useState, useEffect}from 'react';
 import { View, TouchableOpacity, Text, TextInput, ImageBackground, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard} from 'react-native';
-import { useFonts } from 'expo-font';
+import { unloadAllAsync, useFonts } from 'expo-font';
 import * as Font from 'expo-font';
 import {styles} from './LoginScreen.styled'
+import { useForm } from 'react-hook-form';
+// import * as yup  from 'yup';
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isPasswordSecure, setIsPasswordSecure] = useState(true);
   const [keyboardShown, setKeyboardShown] = useState(false);
+  
+  // const validateEmail = () => {
+  //   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  //   if (!emailRegex.test(email)) {
+  //     console.log('Ошибка', 'Некорректный email');
+  //   }
+  // };
+
+  // const validatePassword = () => {
+  //   if (password.length < 6) {
+  //     console.log('Ошибка', 'Пароль должен содержать не менее 6 символов');
+  //   }
+  // };
+
+  // const handleLogin = () => {
+  //   validateEmail();
+  //   validatePassword();
+  // }
 
   useEffect(() => {
     Keyboard.addListener('keyboardDidShow', handleKeyboardDidShow);
@@ -35,12 +57,15 @@ const LoginScreen = () => {
     setPassword('');
   }
   function submitForm (){
-    if(!email || !password){
-      return console.warn('Будь ласка внесіть дані')
+    if(email === " " || password === ""){
+      console.warn('Будь ласка заповніть поля')
     }
- console.log({email, password});
- handleHideBoard();
- clearInputs();
+    if(email !== emailRegex || password.length >0 <6) {
+      console.warn('Будь ласка перевірте заповнені поля')
+    }
+      console.log({email, password});
+      handleHideBoard();
+      clearInputs();
   }
   const [fontsLoaded] = useFonts({
     'Roboto-Bold': require('../../assets/fonts/RobotoCondensed-Bold.ttf'),
@@ -67,6 +92,8 @@ const LoginScreen = () => {
 
                 <TextInput 
                 style={styles.input} 
+                autoComplete="email"
+                autoCapitalize="none"
                 placeholder='Адреса електронної пошти'
                 value={email}
                 onChangeText={(value) => setEmail(value)}
@@ -75,13 +102,18 @@ const LoginScreen = () => {
                <View style={{...styles.passWrapper, marginBottom: keyboardShown ? 323 : 43 }}>
                 <TextInput
                   style={styles.inputLast}
+                  autoCapitalize="none"
                   placeholder="Пароль"
-                  secureTextEntry={isPasswordSecure} value={password} onChangeText={(value) => setPassword(value)}
+                  secureTextEntry={isPasswordSecure} 
+                  value={password}
+                   onChangeText={(value) => setPassword(value)}
                 />
                 <TouchableOpacity style={styles.btnPassShow} onPress ={() => setIsPasswordSecure(!isPasswordSecure)}>
                   <Text style={styles.btnPassShowText}  >{isPasswordSecure ? "Показати" : "Сховати"}</Text>
                 </TouchableOpacity>
+                {/* <View>{errors?.password && <Text>{errors.password.message}</Text> }</View> */}
               </View>
+          
            </KeyboardAvoidingView>
 
             {!keyboardShown && (
